a4d5c40ad2fde0676e7f05b1bf746a650031cd59
DMetaPro <dimitar.yordanov11@outlook.com>
2021-11-17 19:43:10 +0200

    Make all the translations in one class

--- Morse/Morse/Form1.cs
+++ Morse/Morse/Form1.cs
@@ -1,11 +1,7 @@
-using Newtonsoft.Json;
-
 namespace Morse
 {
     public partial class Form1 : Form
     {
-        public string path = "D:/Projects/C#/morse-translator/Morse/Morse/";
-
         public Form1()
         {
             InitializeComponent();
@@ -20,74 +16,7 @@ namespace Morse
         {
             Translator translator = new Translator();
 
-            List<string> input;
-
-            input = translator.removeSpace(textBox1.Text);
-
-            label1.Text = "Start:\n";
-            
-            try
-            {
-                List<Translator> International;
-                List<Translator> Numbers;
-
-                using (StreamReader r = new StreamReader(path + "JSON/International.json"))
-                {
-                    string json = r.ReadToEnd();
-
-                    International = JsonConvert.DeserializeObject<List<Translator>>(json);
-                }
-
-                using (StreamReader r = new StreamReader(path + "JSON/Numbers.json"))
-                {
-                    string json = r.ReadToEnd();
-
-                    Numbers = JsonConvert.DeserializeObject<List<Translator>>(json);
-                }
-
-                foreach (string item in input)
-                {
-                    if (item == " ")
-                    {
-                        label1.Text += item;
-                        label1.Text += "\n";
-                    }
-
-                    foreach (Translator symbol in International)
-                    {
-                        if (symbol.Code == item)
-                        {
-                            label1.Text += symbol.Symbol;
-                            label1.Text += "\n";
-                        }
-                    }
-
-                    foreach (Translator symbol in Numbers)
-                    {
-                        if (symbol.Code == item)
-                        {
-                            label1.Text += symbol.Symbol;
-                            label1.Text += "\n";
-                        }
-                    }
-                }
-            }
-            catch (System.IO.DirectoryNotFoundException)
-            {
-                MessageBox.Show("Необходимите файлове не можаха да бъдат намерени", "Error");
-            }
-            catch (Newtonsoft.Json.JsonReaderException)
-            {
-                MessageBox.Show("Проблем в четенето на JSON файловете.\n" +
-                    "Липстващ елемент.", "Error");
-            }
-
-
-
-
-
-
-            label1.Text += "*";
+            label1.Text = translator.morseToLanguage(textBox1.Text);
         }
     }
 }
--- Morse/Morse/Translator.cs
+++ Morse/Morse/Translator.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Morse
 {
     internal class Translator
     {
+        public string path = "D:/Projects/C#/morse-translator/Morse/Morse/";
         public string Symbol { get; set; }
         public string Code { get; set; }
 
-        public List<string> removeSpace(string input)
+        private List<string> removeSpaceFromMorse(string input)
         {
             List<string> result = new List<string>();
 
@@ -45,6 +47,67 @@ namespace Morse
             }
 
             return result;
+        }
+
+        public String morseToLanguage(string input) // В бъдеще ще приема и език
+        {
+            try
+            {
+                List<Translator> Language;
+                List<Translator> Numbers;
+
+                using (StreamReader r = new StreamReader(path + "JSON/International.json"))
+                {
+                    string json = r.ReadToEnd();
+
+                    Language = JsonConvert.DeserializeObject<List<Translator>>(json);
+                }
+
+                using (StreamReader r = new StreamReader(path + "JSON/Numbers.json"))
+                {
+                    string json = r.ReadToEnd();
+
+                    Numbers = JsonConvert.DeserializeObject<List<Translator>>(json);
+                }
+
+                List<string> array = this.removeSpaceFromMorse(input);
+                string result = "";
+
+                foreach (string item in array)
+                {
+                    if (item == " ")
+                    {
+                        result += item;
+                        result += " ";
+                    }
+
+                    foreach (Translator symbol in Language)
+                    {
+                        if (symbol.Code == item)
+                        {
+                            result += symbol.Symbol;
+                        }
+                    }
+
+                    foreach (Translator symbol in Numbers)
+                    {
+                        if (symbol.Code == item)
+                        {
+                            result += symbol.Symbol;
+                        }
+                    }
+                }
+
+                return result;
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                return "Необходимите файлове не можаха да бъдат намерени";
+            }
+            catch (Newtonsoft.Json.JsonReaderException)
+            {
+                return "Проблем в четенето на JSON файловете.\nЛипстващ елемент.";
+            }
         }
     }
 }
