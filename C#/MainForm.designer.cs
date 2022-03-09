fe43b1abe7fcafbd4127e7542ded98456b5c4412
DMetaPro <dimitar.yordanov11@outlook.com>
2022-03-09 08:35:27 +0200

    Adding icon

--- C#/MainForm.designer.cs
+++ C#/MainForm.designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.selectionBox = new System.Windows.Forms.ComboBox();
             this.translateBtn = new System.Windows.Forms.Button();
             this.inputBox = new System.Windows.Forms.RichTextBox();
@@ -134,6 +135,7 @@
             this.Controls.Add(this.inputBox);
             this.Controls.Add(this.translateBtn);
             this.Controls.Add(this.selectionBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Morse Translator";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
