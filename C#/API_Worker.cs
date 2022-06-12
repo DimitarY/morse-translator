024b3940a9f3b91ed49d9df2c8de19ace96377f2
DimitarY <dimitar.yordanov11@outlook.com>
2022-06-12 22:18:16 +0300

    Adding Class API_Worker

---
+++ C#/API_Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morse_Translator
+{
+    internal class API_Worker
+    {
+        private static API_Worker instance = null;
+        public static API_Worker Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new API_Worker();
+                }
+                return instance;
+            }
+        }
+
+        private string apiURL = "http://127.0.0.1:5000";
+    }
+}
