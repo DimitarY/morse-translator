7eced660a349990d6879a507688e0535f2734b1e
DimitarY <dimitar.yordanov11@outlook.com>
2022-06-12 21:01:11 +0300

    Changing sound settings panel

--- C#/UserControl_Settings.Designer.cs
+++ C#/UserControl_Settings.Designer.cs
@@ -29,13 +29,15 @@
         private void InitializeComponent()
         {
             this.labelFrequency = new System.Windows.Forms.Label();
-            this.textBoxFrequency = new System.Windows.Forms.TextBox();
-            this.textBoxWords_Per_Minute = new System.Windows.Forms.TextBox();
             this.labelWords_Per_Minute = new System.Windows.Forms.Label();
             this.textBoxControl_Word = new System.Windows.Forms.TextBox();
             this.labelControl_Word = new System.Windows.Forms.Label();
             this.sountPanel = new System.Windows.Forms.Panel();
             this.updateSoundButton = new System.Windows.Forms.Button();
+            this.resetSoundButton = new System.Windows.Forms.Button();
+            this.soundPanelLabel = new System.Windows.Forms.Label();
+            this.comboBoxFrequency = new System.Windows.Forms.ComboBox();
+            this.comboBoxWords_Per_Minute = new System.Windows.Forms.ComboBox();
             this.sountPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,76 +46,58 @@
             this.labelFrequency.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.labelFrequency.AutoSize = true;
             this.labelFrequency.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelFrequency.Location = new System.Drawing.Point(14, 18);
+            this.labelFrequency.Location = new System.Drawing.Point(18, 31);
             this.labelFrequency.Name = "labelFrequency";
             this.labelFrequency.Size = new System.Drawing.Size(71, 16);
-            this.labelFrequency.TabIndex = 0;
+            this.labelFrequency.TabIndex = 2;
             this.labelFrequency.Text = "Frequency";
-            // 
-            // textBoxFrequency
-            // 
-            this.textBoxFrequency.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxFrequency.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxFrequency.Location = new System.Drawing.Point(133, 15);
-            this.textBoxFrequency.Name = "textBoxFrequency";
-            this.textBoxFrequency.Size = new System.Drawing.Size(100, 22);
-            this.textBoxFrequency.TabIndex = 0;
-            this.textBoxFrequency.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumbersOnlytextBox_KeyPress);
-            // 
-            // textBoxWords_Per_Minute
-            // 
-            this.textBoxWords_Per_Minute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxWords_Per_Minute.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxWords_Per_Minute.Location = new System.Drawing.Point(133, 43);
-            this.textBoxWords_Per_Minute.Name = "textBoxWords_Per_Minute";
-            this.textBoxWords_Per_Minute.Size = new System.Drawing.Size(100, 22);
-            this.textBoxWords_Per_Minute.TabIndex = 1;
-            this.textBoxWords_Per_Minute.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumbersOnlytextBox_KeyPress);
             // 
             // labelWords_Per_Minute
             // 
             this.labelWords_Per_Minute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.labelWords_Per_Minute.AutoSize = true;
             this.labelWords_Per_Minute.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelWords_Per_Minute.Location = new System.Drawing.Point(14, 46);
+            this.labelWords_Per_Minute.Location = new System.Drawing.Point(18, 60);
             this.labelWords_Per_Minute.Name = "labelWords_Per_Minute";
             this.labelWords_Per_Minute.Size = new System.Drawing.Size(113, 16);
-            this.labelWords_Per_Minute.TabIndex = 3;
+            this.labelWords_Per_Minute.TabIndex = 4;
             this.labelWords_Per_Minute.Text = "Words Per Minute";
             // 
             // textBoxControl_Word
             // 
             this.textBoxControl_Word.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxControl_Word.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxControl_Word.Location = new System.Drawing.Point(133, 71);
+            this.textBoxControl_Word.Location = new System.Drawing.Point(137, 86);
             this.textBoxControl_Word.Name = "textBoxControl_Word";
             this.textBoxControl_Word.Size = new System.Drawing.Size(100, 22);
-            this.textBoxControl_Word.TabIndex = 2;
+            this.textBoxControl_Word.TabIndex = 7;
             // 
             // labelControl_Word
             // 
             this.labelControl_Word.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.labelControl_Word.AutoSize = true;
             this.labelControl_Word.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelControl_Word.Location = new System.Drawing.Point(14, 74);
+            this.labelControl_Word.Location = new System.Drawing.Point(18, 89);
             this.labelControl_Word.Name = "labelControl_Word";
             this.labelControl_Word.Size = new System.Drawing.Size(85, 16);
-            this.labelControl_Word.TabIndex = 5;
+            this.labelControl_Word.TabIndex = 6;
             this.labelControl_Word.Text = "Control Word";
             // 
             // sountPanel
             // 
             this.sountPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sountPanel.Controls.Add(this.comboBoxWords_Per_Minute);
+            this.sountPanel.Controls.Add(this.labelWords_Per_Minute);
+            this.sountPanel.Controls.Add(this.comboBoxFrequency);
+            this.sountPanel.Controls.Add(this.textBoxControl_Word);
+            this.sountPanel.Controls.Add(this.soundPanelLabel);
+            this.sountPanel.Controls.Add(this.resetSoundButton);
             this.sountPanel.Controls.Add(this.updateSoundButton);
             this.sountPanel.Controls.Add(this.labelFrequency);
-            this.sountPanel.Controls.Add(this.textBoxControl_Word);
-            this.sountPanel.Controls.Add(this.textBoxFrequency);
             this.sountPanel.Controls.Add(this.labelControl_Word);
-            this.sountPanel.Controls.Add(this.labelWords_Per_Minute);
-            this.sountPanel.Controls.Add(this.textBoxWords_Per_Minute);
             this.sountPanel.Location = new System.Drawing.Point(5, 5);
             this.sountPanel.Name = "sountPanel";
-            this.sountPanel.Size = new System.Drawing.Size(246, 147);
+            this.sountPanel.Size = new System.Drawing.Size(250, 160);
             this.sountPanel.TabIndex = 0;
             // 
             // updateSoundButton
@@ -124,14 +108,73 @@
             this.updateSoundButton.FlatAppearance.BorderSize = 0;
             this.updateSoundButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.updateSoundButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.updateSoundButton.Location = new System.Drawing.Point(158, 109);
+            this.updateSoundButton.Location = new System.Drawing.Point(162, 122);
             this.updateSoundButton.Margin = new System.Windows.Forms.Padding(2);
             this.updateSoundButton.Name = "updateSoundButton";
             this.updateSoundButton.Size = new System.Drawing.Size(75, 25);
-            this.updateSoundButton.TabIndex = 8;
+            this.updateSoundButton.TabIndex = 1;
             this.updateSoundButton.Text = "Update";
             this.updateSoundButton.UseVisualStyleBackColor = false;
             this.updateSoundButton.Click += new System.EventHandler(this.updateSoundButton_Click);
+            // 
+            // resetSoundButton
+            // 
+            this.resetSoundButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.resetSoundButton.BackColor = System.Drawing.Color.White;
+            this.resetSoundButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.resetSoundButton.FlatAppearance.BorderSize = 0;
+            this.resetSoundButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resetSoundButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resetSoundButton.Location = new System.Drawing.Point(21, 122);
+            this.resetSoundButton.Margin = new System.Windows.Forms.Padding(2);
+            this.resetSoundButton.Name = "resetSoundButton";
+            this.resetSoundButton.Size = new System.Drawing.Size(75, 25);
+            this.resetSoundButton.TabIndex = 0;
+            this.resetSoundButton.Text = "Reset";
+            this.resetSoundButton.UseVisualStyleBackColor = false;
+            this.resetSoundButton.Click += new System.EventHandler(this.resetSoundButton_Click);
+            // 
+            // soundPanelLabel
+            // 
+            this.soundPanelLabel.AutoSize = true;
+            this.soundPanelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.soundPanelLabel.ForeColor = System.Drawing.Color.Gray;
+            this.soundPanelLabel.Location = new System.Drawing.Point(0, 0);
+            this.soundPanelLabel.Name = "soundPanelLabel";
+            this.soundPanelLabel.Size = new System.Drawing.Size(95, 16);
+            this.soundPanelLabel.TabIndex = 8;
+            this.soundPanelLabel.Text = "Sound settings";
+            // 
+            // comboBoxFrequency
+            // 
+            this.comboBoxFrequency.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxFrequency.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxFrequency.FormattingEnabled = true;
+            this.comboBoxFrequency.Items.AddRange(new object[] {
+            "200",
+            "550",
+            "1000"});
+            this.comboBoxFrequency.Location = new System.Drawing.Point(137, 28);
+            this.comboBoxFrequency.Name = "comboBoxFrequency";
+            this.comboBoxFrequency.Size = new System.Drawing.Size(100, 23);
+            this.comboBoxFrequency.TabIndex = 4;
+            // 
+            // comboBoxWords_Per_Minute
+            // 
+            this.comboBoxWords_Per_Minute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxWords_Per_Minute.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxWords_Per_Minute.FormattingEnabled = true;
+            this.comboBoxWords_Per_Minute.Items.AddRange(new object[] {
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60"});
+            this.comboBoxWords_Per_Minute.Location = new System.Drawing.Point(137, 57);
+            this.comboBoxWords_Per_Minute.Name = "comboBoxWords_Per_Minute";
+            this.comboBoxWords_Per_Minute.Size = new System.Drawing.Size(100, 23);
+            this.comboBoxWords_Per_Minute.TabIndex = 9;
             // 
             // UserControl_Settings
             // 
@@ -154,12 +197,14 @@
         #endregion
 
         private System.Windows.Forms.Label labelFrequency;
-        private System.Windows.Forms.TextBox textBoxFrequency;
-        private System.Windows.Forms.TextBox textBoxWords_Per_Minute;
         private System.Windows.Forms.Label labelWords_Per_Minute;
         private System.Windows.Forms.TextBox textBoxControl_Word;
         private System.Windows.Forms.Label labelControl_Word;
         private System.Windows.Forms.Panel sountPanel;
         private System.Windows.Forms.Button updateSoundButton;
+        private System.Windows.Forms.Button resetSoundButton;
+        private System.Windows.Forms.Label soundPanelLabel;
+        private System.Windows.Forms.ComboBox comboBoxFrequency;
+        private System.Windows.Forms.ComboBox comboBoxWords_Per_Minute;
     }
 }
--- C#/UserControl_Settings.cs
+++ C#/UserControl_Settings.cs
@@ -23,12 +23,14 @@ namespace Morse_Translator
         }
 
         private Sound sound;
+        private Settings settings;
 
         public UserControl_Settings()
         {
             InitializeComponent();
             
             sound = Sound.Instance;
+            settings = Settings.Instance;
         }
 
         private void UserControl_Trainer_Load(object sender, EventArgs e)
@@ -38,21 +40,49 @@ namespace Morse_Translator
 
         private void UserControl_Trainer_Enter(object sender, EventArgs e)
         {
-            textBoxFrequency.Text = sound.Frequency.ToString();
-            textBoxWords_Per_Minute.Text = sound.WPM.ToString();
+            comboBoxFrequency.Text = sound.Frequency.ToString();
+            comboBoxWords_Per_Minute.Text = sound.WPM.ToString();
             textBoxControl_Word.Text = sound.CodeWord.ToString();
         }
 
         private void NumbersOnlytextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsDigit(e.KeyChar)) e.Handled = true;
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar == (char)Keys.Return && e.KeyChar == (char)Keys.Back) e.Handled = true;
         }
 
-        private void updateSoundButton_Click(object sender, EventArgs e)
+        private void updateSoundButton_Click(object sender, EventArgs e) //TODO: при отваряне на падащото меню, автоматично се попълва стойност с написаното начално шисло
         {
-            sound.Frequency = ushort.Parse(textBoxFrequency.Text);
-            sound.WPM = ushort.Parse(textBoxWords_Per_Minute.Text);
-            sound.CodeWord = textBoxControl_Word.Text;
+
+            if (!String.IsNullOrEmpty(comboBoxFrequency.Text) &&
+                !String.IsNullOrEmpty(comboBoxWords_Per_Minute.Text) &&
+                !String.IsNullOrEmpty(textBoxControl_Word.Text))
+            {
+                if (ushort.Parse(comboBoxFrequency.Text) >= 200 && ushort.Parse(comboBoxFrequency.Text) <= 44000 &&
+                    ushort.Parse(comboBoxWords_Per_Minute.Text) >= 1 && ushort.Parse(comboBoxWords_Per_Minute.Text) <= 60)
+                {
+                    sound.Frequency = ushort.Parse(comboBoxFrequency.Text);
+                    sound.WPM = ushort.Parse(comboBoxWords_Per_Minute.Text);
+                    sound.CodeWord = textBoxControl_Word.Text;
+
+                    settings.saveSound();
+                    sound.setWaveOut();
+
+                    if ((sender as Button).Name == updateSoundButton.Name) MessageBox.Show("Sound settings updated.", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else MessageBox.Show("Frequency must be between 200 and 44000 Hz and Words Per Minute must be greater than 1 and less than 60.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else MessageBox.Show("Please enter all fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void resetSoundButton_Click(object sender, EventArgs e)
+        {
+            comboBoxFrequency.SelectedIndex = 1;
+            comboBoxWords_Per_Minute.SelectedIndex = 1;
+            textBoxControl_Word.Text = "PARIS";
+
+            this.updateSoundButton_Click(sender, e);
+
+            MessageBox.Show("Sound settings reset to default.", "Reset", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
